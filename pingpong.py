dbc26350d73dd155d8bb199e5a6f10655d88547e
username123321246 <george.mircea.vrana@gmail.com>
2025-09-30 18:00:25 +0200

    Update pingpong.py

--- pingpong.py
+++ pingpong.py
@@ -7,36 +7,149 @@ root = Tk()
 root.title("Ping Pong game")
 root.resizable(0, 0)
 root.wm_attributes("-topmost", 1)
-canvas = Canvas(root, width = 600, height = 300, bd = 2, highlightthickness = 3)
+canvas = Canvas(root, width = 500, height = 300, bd = 2, highlightthickness = 3)
 canvas.config(bg = "black")
 canvas.pack()
 
 car = canvas.create_text(300, 20, fill = "white", font = ("bold", 40), text = ":")
 root.update()
 # x1, y1    x2, y2
-canvas.create_line(300, 0, 300, 600, fill = "white", opacity = 0.5)
+canvas.create_line(300, 0, 300, 600, fill = "white")
+
+leftpaddlescore = 0
+rightpaddlescore = 0
+
+score_font = ("Calibri", 10)
+score_text = (canvas.create_text(300, 20, font = score_font, text = "0:0"))
+# Create circle at the center of the canvas
+x = 300
+y = 250
+r = 50
+
+class Ball:
+    def __init__(self, canvas, paddle1, paddle2, color):
+        self.id = canvas.create_oval(10, 10, 30, 30, fill = color)
+        self.canvas.move(self.id, 280, 300)
+
+# Randomizes ball’s start direction and speed for both X and Y axes.
+        start = [-3, -2, -1, 1, 2, 3 ]
+        random.shuffel(start)
+        self.x = start[1]
+        self.y = start[2]
+
+        self.canvas_height = self.canvas.winfo_height() 
+        self.canvas_width = self.winfo_width()   
+
+        self.score1 = 0
+        self.score2 = 0
+    
+    def draw(self):
+        global leftpaddlescore, rightpaddlescore
+        self.canvas.move(self.id, self.x, self.y)
+        pos = self.canvas.coords(self.id)
+
+        if pos [1] <= 0:      # The top of the ball touches the top of the  game screen which is 0
+            self.y = 4
+        if pos [3] >= self.canvas_height:     # The bottom most of the ball touches the bottom of the game screen
+            self.y = -4
+        if pos [2] >= self.canvas_width:
+            self.x = -4
+            leftpaddlescore += 1
+            canvas.itemconfigure(score_text, text = str(leftpaddlescore) + ":" + str(rightpaddlescore))
+        if pos [0] <= 0:
+            self.x = 4
+            rightpaddlescore += 1
+            canvas.itemconfigure(score_text, text = str(leftpaddlescore) + ":" + str(rightpaddlescore))
+            
+    def hit_paddle1(self, pos):
+        paddle_pos = self.canvas(self.paddle1.id)
+        if pos[1] >= paddle_pos[1] and pos[1] <= paddle_pos[3]:
+            if pos[0] >= paddle_pos[0] and pos[0] <= paddle_pos[2]:
+                return True
+            
+            return False
+
+    def hit_paddle2(self, pos):
+        paddle_pos2 = self.canvas(self.paddle2.id)
+        if pos[1] >= paddle_pos2[1] and pos[1] <= paddle_pos2[3]:
+            if pos[0] >= paddle_pos2[0] and pos[0] <= paddle_pos2[2]:
+                return True
+            
+            return False
+
+class Paddle1:
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = canvas.create_rectangle(10, 125, 20, 40)
+        self.y = 0
+        self.canvas_height = self.canvas.winfo_height()
+        self.canvas_width = self.canvas.winfo_width()
+        self.canvas.bind_all("w", self.move_up)
+        self.canvas.bind_all("s", self.move_down)
+
+    def draw(self):
+        self.canvas.move(self.id, 0, self.y)
+        pos = self.canvas.coords(self.id)
+        if pos[1] <= 0:
+            self.y = 0
+        if pos[3] >= self.canvas_height:
+            self.y = 0
 
 
 
 
 
+    def move_up(self, event):
+        self.y = -4
+    
+    def move_down(self, event):
+        self.y = 4
+
+
+
+class Paddle2:
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = canvas.create_rectangle(490, 125, 20, 40)
+        self.y = 0
+        self.canvas_height = self.canvas.winfo_height()
+        self.canvas_width = self.canvas.winfo_width()
+        self.canvas.bind_all("<KeyPress-Up>", self.move_up)
+        self.canvas.bind_all("<KeyPress-Down>", self.move_down)
+
+    def draw(self):
+        self.canvas.move(self.id, 0, self.y)
+        pos = self.canvas.coords(self.id)
+        if pos[1] <= 0:
+            self.y = 0
+        if pos[3] >= self.canvas_height:
+            self.y = 0
 
 
 
 
 
+    def move_up(self, event):
+        self.y = -4
+    
+    def move_down(self, event):
+        self.y = 4
+
+paddle1 = Paddle1(canvas, "orange")
+paddle2 = Paddle2(canvas, "teal")
+ball = Ball(canvas, paddle1, paddle2, "yellow")
 
 
 
 
-
-
-
-
-
-
-
+while True:
+    paddle1.draw()
+    paddle2.draw()
+    ball.draw()
 
 
 root.mainloop()
+
+
+
 
